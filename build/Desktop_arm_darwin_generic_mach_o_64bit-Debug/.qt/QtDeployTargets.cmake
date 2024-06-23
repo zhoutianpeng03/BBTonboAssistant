51a92b4bb629b48ce2097913eb4a7c5c3d03e8f7
zhoutianpeng01 <zhoutianpeng@shoppal.ai>
2024-06-23 20:23:20 +0800

    feat:remove build dir

--- build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_BBTonboAssitant_FILE /Users/zhoutianpeng/Muge/project/BBTonboAssitant/build/Desktop_arm_darwin_generic_mach_o_64bit-Debug/BBTonboAssitant.app/Contents/MacOS/BBTonboAssitant)
-set(__QT_DEPLOY_TARGET_BBTonboAssitant_TYPE EXECUTABLE)
